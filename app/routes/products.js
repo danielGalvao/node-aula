f8dac1c153e27fdc09b41d60630b2af13e169994
Daniel Galvão <galvaodev@gmail.com>
2016-05-18 00:28:37 -0300

    Add mysql module and selecting books

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,5 +1,15 @@
 module.exports = (app) => {
-  app.get("/produtos", (req,res)=>{
-    res.render("product/lista")
+  app.get("/produtos", (req,res) => {
+    var appMysql = require("mysql");
+    var conn = appMysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'livraria'
+    });
+    conn.query("SELECT * FROM livros",function(err, resp){
+      res.send(resp);
+    });
+    conn.end();
   });
 }
