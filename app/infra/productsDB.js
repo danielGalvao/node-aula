aec70aa5814637fa94a40c9938430272124c1196
Daniel Galvão <galvaodev@gmail.com>
2016-06-02 00:14:01 -0300

    functions to save product

--- app/infra/productsDB.js
+++ app/infra/productsDB.js
@@ -5,6 +5,10 @@ ProductsDB.prototype.list = function(callback){
   this.conn.query("SELECT * FROM livros",callback);
 }
 
+ProductsDB.prototype.save = function(product,callback){
+    this.conn.query("INSERT INTO livros set ?",product,callback);
+}
+
 module.exports = function(){
   return ProductsDB;
 }
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,14 +1,29 @@
 
 module.exports = (app) => {
-  app.get("/produtos", (req,res) => {
 
+  var productList = (req,res) => {
     var conn = app.infra.dbConnection();
     var prodDB = new app.infra.productsDB(conn);
-
-    prodDB.list(function(err, resp){
+    prodDB.list((err, resp) => {
       res.render("product/list",{lista:resp});
     });
-
     conn.end();
+  }
+  // Lista
+  app.get("/produtos", productList);
+
+  // Cadastro
+  app.get("/produtos/new", (req,res) => {
+    res.render("product/form");
+  });
+
+  // Salva livro
+  app.post("/products/save", (req,res) => {
+    var conn = app.infra.dbConnection();
+    var prodDB = new app.infra.productsDB(conn);
+    var product = res.body;
+    prodDB.save(product,(err,resp) => {
+      resp.redirect("/produtos");
+    });
   });
 }
