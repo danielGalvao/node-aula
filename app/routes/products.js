b2d6da11b00c0a6068570f1b052630b4da26a276
Daniel Galvão <galvaodev@gmail.com>
2016-06-22 23:37:44 -0300

    Add validate form

--- app/routes/products.js
+++ app/routes/products.js
@@ -21,14 +21,24 @@ module.exports = (app) => {
 
   // Cadastro
   app.get("/produtos/new", (req,res) => {
-    res.render("product/form");
+    res.render("product/form",{errorsValidate:{}});
   });
 
   // Salva livro
   app.post("/produtos", (req,res) => {
+    var product = req.body;
+    var checkTitle = req.assert('titulo', "Titulo e obrigatorio").notEmpty();
+    var checkPrice = req.assert('preco', "Formato invalido").isFloat();
+    var errors = req.validationErrors();
+    console.log(errors);
+    if(errors){
+      res.render("product/form",{errorsValidate:errors});
+      return;
+    }
+
     var conn = app.infra.dbConnection();
     var prodDB = new app.infra.productsDB(conn);
-    var product = req.body;
+
     prodDB.save(product,(err,resp) => {
       res.redirect("/produtos");
     });
