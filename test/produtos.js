c0fa781fd589b900c51c9cd3ea3ead41a979d632
Daniel Galvão <galvaodev@gmail.com>
2016-08-16 01:06:39 -0300

    Working with mocha

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,22 @@
-describe("#ProdutosController",function(){
+var http = require('http');
+describe("#ProdutosController",function(funcaoFinalizacao){
   it("#listagem JSON",function(){
-    console.log('teste JSON');
+    var configuracoes = {
+        hostname: 'localhost',
+        port: 3001,
+        path: '/produtos',
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+    http.get(configuracoes, function(res){
+      if(res.statusCode == 200){
+        console.log('Status OK');
+      }
+      if(res.headers['content-type'] == 'application/json;charset=utf-8'){
+        console.log('content-type OK');
+      }
+      funcaoFinalizacao();
+    });
   });
 });
