67aa0fb15770a9f038d353686496e85f4829070c
Daniel Galvão <galvaodev@gmail.com>
2016-09-02 22:33:59 -0300

    Remove process env

--- app/infra/dbConnection.js
+++ app/infra/dbConnection.js
@@ -1,31 +1,13 @@
 var appMysql = require("mysql");
 var connectDB = function(){
-  if(!process.env.NODE_ENV){
-    return appMysql.createConnection({
-      host     : 'localhost',
-      user     : 'root',
-      password : '',
-      database : 'livraria'
-    });
-  }
-  if(process.env.NODE_ENV == 'test'){
-    return appMysql.createConnection({
-      host     : 'localhost',
-      user     : 'root',
-      password : '',
-      database : 'livraria'
-    });
-  }
-  if(process.env.NODE_ENV == 'production'){
-    var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
-    var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)?reconnect=true/);
-    return appMysql.createConnection({
-      host     : grupos[3],
-      user     : grupos[1],
-      password : grupos[2],
-      database : grupos[4]
-    });
-  }
+  var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+  var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)?reconnect=true/);
+  return appMysql.createConnection({
+    host     : grupos[3],
+    user     : grupos[1],
+    password : grupos[2],
+    database : grupos[4]
+  });
 }
 module.exports = function(){
   return connectDB;
