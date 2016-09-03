db5c87138bca336cfb9e8b22c35c79fe69aaeb87
Daniel Galvão <galvaodev@gmail.com>
2016-09-02 22:57:01 -0300

    Fix html code

--- app/infra/dbConnection.js
+++ app/infra/dbConnection.js
@@ -2,12 +2,11 @@ var appMysql = require("mysql");
 var connectDB = function(){
   var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
   var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)?reconnect=true/);
-  console.log(grupos);
   return appMysql.createConnection({
-    host     : "us-cdbr-iron-east-04.cleardb.net",
+    host     : grupos[3],
     user     : grupos[1],
     password : grupos[2],
-    database : grupos[3]
+    database : grupos[4]
   });
 }
 module.exports = function(){
